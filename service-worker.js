ae9f6588fddb559919947c795724014bc49086cd
Belardino-Capessa <belardinocapessa@gmail.com>
2026-06-21 09:20:55 +0100

    Atualizar o service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,12 +1,44 @@
-self.addEventListener("install", () => {
-  console.log("Service Worker instalado");
+const CACHE_NAME = "financeflow-v1";
+
+const FILES_TO_CACHE = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./assets/icons/icon-192.png",
+  "./assets/icons/icon-512.png"
+];
+
+// INSTALL
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(FILES_TO_CACHE);
+    })
+  );
   self.skipWaiting();
 });
 
-self.addEventListener("activate", () => {
-  console.log("Service Worker ativado");
+// ACTIVATE
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.map((key) => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      );
+    })
+  );
+  self.clients.claim();
 });
 
+// FETCH (OFFLINE FIRST)
 self.addEventListener("fetch", (event) => {
-  event.respondWith(fetch(event.request));
+  event.respondWith(
+    caches.match(event.request).then((cached) => {
+      return cached || fetch(event.request);
+    })
+  );
 });
